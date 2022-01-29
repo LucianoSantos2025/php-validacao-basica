9a43df2b0719d4e8ffdc3e3e2be82211422c795c
LucianoSantos2025 <lucsantos.2025@gmail.com>
2022-01-29 13:12:24 -0300

    criado validacao basica

---
+++ validacaoBasica.php
@@ -0,0 +1,23 @@
+<?php
+$nome1 = "Pedro";
+
+echo $nome1;
+echo "<br>";
+
+// destroi/ anula a variavel - comando unset -passo1
+
+	unset($nome1);	
+	
+/*
+validacao basica se ela existe? -passo2 (isset)
+se a variavel tiver sido declarada ela sera apresentada - 
+- anula o unset caso seja colocada como comentario
+*/
+
+	if(isset($nome1)){
+		echo $nome1; }
+	else {
+		echo "variável não existe";
+	}
+
+?>
