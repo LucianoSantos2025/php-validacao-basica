8115d8c783bd1d62795d3068be6b4121eda396ba
Luciano  Silva Santos <lucsantos.2025@gmail.com>
2022-01-30 22:18:22 -0300

    Update validacaoBasica.php

--- validacaoBasica.php
+++ validacaoBasica.php
@@ -1,23 +1,22 @@
 <?php
-$nome1 = "Pedro";
 
+$nome1 = "Pedro";
 echo $nome1;
 echo "<br>";
 
-// destroi/ anula a variavel - comando unset -passo1
-
-	unset($nome1);	
+/*	passo1- comando unset - destroi/anula a variavel.
+Observacao: para anular o unset - a linha de codigo abaixo 
+devera ser colocada como comentario	*/
+unset($nome1);	
 	
-/*
-validacao basica se ela existe? -passo2 (isset)
-se a variavel tiver sido declarada ela sera apresentada - 
-- para anular o unset - coloque o unset como comentario
-*/
+/*	passo2 (isset)- validacao basica - para verificar se a variavel existe? 
+se a variavel tiver sido declarada ela sera apresentada.
+Como a variavel foi anulada pelo o unset o reultado sera que ela nao existe!	*/
 
-	if(isset($nome1)){
-		echo $nome1; }
-	else {
-		echo "variável não existe";
+if(isset($nome1)){
+	echo $nome1; }
+else {
+	echo "variável não existe";
 	}
 
 ?>
